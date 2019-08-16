72663300354682da08eb40840e36e176aa95a241
abhilashw <wankarabhilash1999@gmail.com>
2019-08-16 16:06:27 +0530

    added renderboard

--- GameOfLife.java
+++ GameOfLife.java
@@ -47,19 +47,44 @@ public class GameOfLife{
 		return random ;
 	}
 
+	public List<List<Character>> renderBoard(List<List<Integer>> random, int width,int height){
+
+		List<List<Character>> board =new ArrayList<List<Character>>();
+		int i=0;
+		while(i<height){
+			List<Integer> inside = random.get(i);
+			List<Character> ins= new ArrayList<>();
+			int j=0;
+			while(j< width){
+				
+				if(inside.get(j)==1){
+					ins.add('#');
+				}
+				else{
+					ins.add(' ');
+				}
+				j++;
+			}
+			board.add(ins);	
+			i++;
+		}
+		return board ;
+	}
+
 	public static void main(String[] args){
 
-		GameOfLife system1=new GameOfLife(5,5);
-		List<List<Integer>> res=system1.randomState(5,5);
+		GameOfLife system1=new GameOfLife(20,20);
+		List<List<Integer>> res=system1.randomState(20,20);
+		List<List<Character>> b =system1.renderBoard(res,20,20);
 
-		System.out.println(res);
+		// System.out.println(res);
 		
 		
-		// for(int i=0;i<system1.height ;i++){
-		// 	for(int j=0;j<system1.width ;j++){
-		// 		System.out.printf("%d ",res.get(i).get(j));
-		// 	}
-		// 	System.out.println();
-		// }
+		for(int i=0;i<b.size() ;i++){
+			for(int j=0;j<b.get(i).size() ;j++){
+				System.out.printf("%c ",b.get(i).get(j));
+			}
+			System.out.println();
+		}
 	}
 }
